e706497e34ba2e26a3bcd2064c10787e16ae14ec
Остап <jarymorfifa@gmail.com>
2023-07-07 20:04:50 +0300

    Homework 16 is done.

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -1,7 +1,12 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
+import com.skillbox.airport.Terminal;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,7 +16,13 @@ public class Main {
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
         //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+        int twoHours = 7_200_000;
+
+        return airport.getTerminals().stream()
+                .map(Terminal::getFlights)
+                .flatMap(List::stream)
+                .filter(f -> f.getType() == Flight.Type.DEPARTURE && f.getDate().getTime() > System.currentTimeMillis()
+                && f.getDate().getTime() < System.currentTimeMillis() + twoHours).collect(Collectors.toList());
     }
 
 }
