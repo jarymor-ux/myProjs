a308fe1694d967cccba01f2765ec0242a954a8f4
jarymor-ux <120585847+jarymor-ux@users.noreply.github.com>
2023-08-26 14:29:34 +0300

    HW is done.

--- Multithreading/SIteMap/src/main/java/src/Main.java
+++ Multithreading/SIteMap/src/main/java/src/Main.java
@@ -1,100 +1,74 @@
 package src;
 
-import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalTime;
+import java.util.concurrent.ForkJoinPool;
 
-public class Main
-{
-    static Parser parser = null;
+public class Main {
 
-    public static void main(String[] args)
-    {
-        parseSkillbox();
-        saveLinks();
-    }
-
-
-
-    public static void parseSkillbox()
-    {
-        ArrayList<String> allowedDomains = new ArrayList<>();
-        allowedDomains.add("skillbox.ru");
-
-        String cachePath = "./data";
-        int maxThreads = 3;
-
-        parser = new Parser(allowedDomains, cachePath, maxThreads);
-        parser.run("https://skillbox.ru/");
-    }
+    private static String url;
+    private static String fileName;
+    private static final String DST_FOLDER = "src/main/resources/sitemap/";
+    private static final String FILE_EXTENSION = "txt";
+    private static int numberOfCores = Runtime.getRuntime().availableProcessors();
+    private static int numberOfThreads;
+    private static long startOfTime;
 
 
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Введите адрес сайта для сканирования: =>> Согласно примера: https://lenta.ru");
+            url = reader.readLine();
+            fileName = new URL(url).getHost().replace(".", "_");
 
-    public static void saveLinks()
-    {
-        System.out.println("Сохранить список ссылок в файл");
-        try
-        {
-            FileWriter file;
-            file = new FileWriter("C:\\dst\\skillbox.txt");
+            System.out.println("Введите количество создаваемых потоков: \n\t=>> 0 - согласно количеству ядер процессора");
+            numberOfThreads = Integer.parseInt(reader.readLine());
 
-            HashSet<String> urls = new HashSet<>();
-            List<Page> pages = parser.getPages();
-            pages.sort((Page page1, Page page2) -> {
-                return Page.compareLinks(page1.getFullUrl(), page2.getFullUrl());
-            });
+            System.out.println("Начато сканирование сайта: " + LocalTime.now());
+            startOfTime = System.currentTimeMillis();
 
-            for (int i=0; i<pages.size(); i++)
-            {
-                Page page = pages.get(i);
-                String url = page.getFullUrl();
+            LinkExecutor linkExecutor = new LinkExecutor(url);
+            String siteMap = new ForkJoinPool(numberOfThreads == 0 ? numberOfCores : numberOfThreads).invoke(linkExecutor);
 
+            System.out.println("Закончено сканирование сайта: " + LocalTime.now());
+            long periodOfTimeInSec = (System.currentTimeMillis() - startOfTime) / 1000;
 
-                if (url.length() == 0)
-                {
-                    continue;
-                }
+            System.out.println("Время сканирования составило: " + periodOfTimeInSec + " сек.");
 
+            writeToFile(siteMap);
 
-                if (!page.isValid())
-                {
-                    continue;
-                }
+        } catch (IOException e) {
 
+        } catch (IllegalArgumentException e) {
 
-                int pos = page.getFullUrl().indexOf("?");
-                if (pos >= 0)
-                {
-                    url = url.substring(0, pos);
-                }
-
-
-                if (url.charAt(url.length() - 1) != '/')
-                {
-                    url += "/";
-                }
-
-
-                if (urls.contains(url))
-                {
-                    continue;
-                }
-
-                urls.add(url);
-
-                int level = Page.getLevelUrl(url);
-                String s = "\t".repeat(level) + page.getFullUrl();
-                file.write(s + "\n");
-            }
-
-            file.flush();
-            file.close();
-        }
-        catch (IOException e)
-        {
+            System.err.println("Превышено допустимое количество, введите другое значение!");
         }
     }
 
+    private static void writeToFile(String siteMap) {
+        System.out.println("Производится запись данных в файл ==> ==> ==>");
+        if (!Files.exists(Paths.get(DST_FOLDER))) {
+            new File(DST_FOLDER).mkdir();
+        }
+        String filePath = DST_FOLDER.concat(fileName).concat(".").concat(FILE_EXTENSION);
+        File file = new File(filePath);
+        try (PrintWriter writer = new PrintWriter(file)) {
+            writer.write(siteMap);
+            writer.flush();
+        } catch (FileNotFoundException e) {
+
+        }
+        System.out.println("Файл записан:");
+        long periodOfTimeInSec = (System.currentTimeMillis() - startOfTime) / 1000;
+        System.out.println(file.getAbsolutePath());
+
+    }
 }
