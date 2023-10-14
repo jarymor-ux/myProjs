69e78e69c7af4a55f29039dc7a3ae0b7041db5b5
jarym <jarymorfifa@gmail.com>
2023-10-15 01:43:41 +0300

    HW is done.

---
+++ SpringBootWebDevelopment/ToDoList/src/main/java/main/Controller.java
@@ -0,0 +1,77 @@
+package main;
+
+import main.Model.Task;
+import main.Model.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class Controller {
+    @Autowired
+    private TaskRepository taskRepository;
+    @PostMapping("/tasks")
+    public ResponseEntity<Void> createTask(@RequestBody Task task) {
+        task.setCreationTime(LocalDateTime.now());
+        task.setDone(false);
+
+        taskRepository.save(task);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/tasks/{ID}")
+    public ResponseEntity getTask(@PathVariable int ID){
+        Optional<Task> task = taskRepository.findById(ID);
+        if (taskRepository.findById(ID).isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        System.out.println(task);
+        return new ResponseEntity(task, HttpStatus.OK);
+    }
+
+    @GetMapping("/tasks")
+    public ResponseEntity<List<Task>> getAllTasks(){
+        List<Task> tasks = (List<Task>) taskRepository.findAll();
+
+        return ResponseEntity.ok(tasks);
+    }
+
+    @PatchMapping("/tasks/{ID}")
+    public ResponseEntity<Task> updateTask(@PathVariable int ID, @RequestBody Task task) {
+        Task existingTask = taskRepository.findById(ID).orElseThrow(() -> new ResourceNotFoundException("Task not found with id: " + ID));
+
+
+        if (task.getTitle() != null) {
+            existingTask.setTitle(task.getTitle());
+        }
+        if (task.getDescription() != null) {
+            existingTask.setDescription(task.getDescription());
+        }
+        if (task.isDone()) {
+            existingTask.setDone(task.isDone());
+        }
+
+        taskRepository.save(existingTask);
+
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+
+    @DeleteMapping("/tasks/{ID}")
+    public ResponseEntity<Task> deleteTask(@PathVariable int ID){
+
+        if (taskRepository.findById(ID).isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        } else {
+            taskRepository.deleteById(ID);
+            return ResponseEntity.status(HttpStatus.OK).body(null);
+        }
+    }
+
+}
---
+++ SpringBootWebDevelopment/ToDoList/src/main/java/main/Model/Task.java
@@ -0,0 +1,57 @@
+package main.Model;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private LocalDateTime creationTime;
+    private boolean isDone;
+    private String title;
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(LocalDateTime creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
